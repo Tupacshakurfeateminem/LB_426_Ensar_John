46af909b7d2de7acb2a26b58ff4b2d3fa6e75f86
John <broderjohn5@gmail.com>
2024-05-01 14:13:18 +0200

    Projektdateien hinzufügen.

---
+++ LB_426_Emsar_John/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LB_426_Emsar_John
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
