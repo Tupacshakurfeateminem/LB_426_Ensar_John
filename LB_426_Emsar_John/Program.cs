bc208843b033171fc2003592e60476c73cecc0be
John <broderjohn5@gmail.com>
2024-05-01 14:32:18 +0200

    Geld kann in Jetons umgewandelt werden

--- LB_426_Emsar_John/Program.cs
+++ LB_426_Emsar_John/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace LB_426_Emsar_John
+﻿namespace LB_426_Emsar_John;
+using System;
+
+// Hauptklasse für das Casino
+public class Casino
 {
-    internal class Program
+    private int jetons; // Jetons des Benutzers
+
+    public Casino()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        jetons = 0; // Start mit 0 Jetons
+    }
+
+    // Methode zum Umtausch von Geld in Jetons
+    public void GeldZuJetons(int geld)
+    {
+        jetons += geld;
+        Console.WriteLine($"Sie haben {geld} Euro in Jetons umgetauscht. Aktueller Jeton-Guthaben: {jetons}");
+    }
+
+    // Hauptmethode
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Willkommen im Casino!");
+        Console.WriteLine("Bitte geben Sie den Betrag in Euro ein, den Sie in Jetons umtauschen möchten:");
+
+        int eingabeBetrag = Convert.ToInt32(Console.ReadLine());
+
+        Casino casino = new Casino();
+        casino.GeldZuJetons(eingabeBetrag);
     }
 }
